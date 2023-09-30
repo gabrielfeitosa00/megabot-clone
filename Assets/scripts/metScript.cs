fae1a71d6913eedf10399aa7f05c41b5e3cc1d88
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2023-09-30 13:49:27 -0300

    end stage

--- Assets/scripts/metScript.cs
+++ Assets/scripts/metScript.cs
@@ -14,6 +14,7 @@ public class metScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+      
         if (  col.collider.gameObject.layer == 7)
         {
             Destroy(gameObject);
